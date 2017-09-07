2663ba8eebeba189a364ca15b35c89e670e3e1cc
Myko Bova <mykola@backbase.com>
2017-09-07 13:10:15 +0300

    first commit

---
+++ scaling-java-applications-through-concurrency/3-scaling-java-applications-through-concurrency-m3-exercise-files/identity-pipeline/src/com/joshcummings/codeplay/concurrency/splitting/ScatterGatherer.java
@@ -0,0 +1,20 @@
+package com.joshcummings.codeplay.concurrency.splitting;
+
+import java.util.concurrent.Callable;
+
+import com.joshcummings.codeplay.concurrency.Identity;
+
+public interface ScatterGatherer {
+	public Identity go(Scatterer s, Gatherer g);
+	
+	public interface Scatterer {
+		boolean hasNext();
+		Callable<Identity> next();
+	}
+	
+	public interface Gatherer {
+		boolean needsMore();
+		void gatherResult(Identity i);
+		Identity getFinalResult();
+	}
+}
